0aa09f7dbdafc24c54c17355ccc018a1b122a417
Victor Mellado Gomez <contacto@victormellado.cl>
2023-10-11 09:31:46 -0300

    form ok

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,5 @@
 import { quizAnswers } from './quizdata.js';
+import { vkQuizappState } from './vkQuizappState.js';
 
 const evalAnswer = (questionId, answer) => {
   const result = (quizAnswers[questionId] == answer);
@@ -6,18 +7,7 @@ const evalAnswer = (questionId, answer) => {
 };
 
 function captureSubmitEvents() {
-    const forms = document.querySelectorAll('form'); // Selecciona todos los formularios de la página
-  
-    forms.forEach((form) => {
-      form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Evita que el formulario se envíe y se recargue la página
-  
-        const formData = new FormData(form); // Obtiene los datos del formulario
-        const data = Object.fromEntries(formData.entries()); // Convierte los datos en un objeto
-  
-        console.log('Datos del formulario:', data); // Muestra los datos en la consola
-      });
-    });
+
   }
 
 
