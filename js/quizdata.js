a9291fab3738be973a61197b121bb0e4f5d1227c
Victor Mellado Gomez <contacto@victormellado.cl>
2023-10-12 12:18:27 -0300

    paso.

---
+++ js/quizdata.js
@@ -0,0 +1,62 @@
+const myQuestions = [
+    {
+        question: "¿de qué color es la naranja?",
+        answers: {
+            a: "verde",
+            b: "azul",
+            c: "naranja",
+            d: "anaranjada",
+
+        },
+        correctAnswers: ["a", "b"],
+        questionType: "multiple-choice"
+    },
+    {
+        question: "¿de qué color es el plátano?",
+        answers: {
+            a: "verde",
+            b: "azul",
+            c: "naranja",
+            d: "amarillo"
+        },
+        correctAnswers: ["b"],
+        questionType: "single-choice"
+    },
+    {
+        question: "¿de qué color es el plátano?2",
+        answers: {
+            a: "verde",
+            b: "azul",
+            c: "naranja",
+            d: "amarillo"
+        },
+        correctAnswers: ["b"],
+        questionType: "single-choice"
+    },
+    {
+        question: "¿de qué color es el plátano?3",
+        answers: {
+            a: "verde",
+            b: "azul",
+            c: "naranja",
+            d: "amarillo"
+        },
+        correctAnswers: ["b"],
+        questionType: "single-choice"
+    },
+    {
+        question: "¿de qué color es el plátano?4",
+        answers: {
+            a: "verde",
+            b: "azul",
+            c: "naranja",
+            d: "amarillo"
+        },
+        correctAnswers: ["b"],
+        questionType: "single-choice"
+    },
+    // Agrega más preguntas aquí
+];
+
+
+export{myQuestions};
--- js/script.js
+++ js/script.js
@@ -1,78 +1,21 @@
-const myQuestions = [
-    {
-        question: "¿de qué color es la naranja?",
-        answers: {
-            a: "verde",
-            b: "azul",
-            c: "naranja",
-            d: "anaranjada",
-            e: "anaranjada",
-            f: "anaranjada"
-        },
-        correctAnswers: ["a", "b"],
-        questionType: "multiple-choice"
-    },
-    {
-        question: "¿de qué color es el plátano?",
-        answers: {
-            a: "verde",
-            b: "azul",
-            c: "naranja",
-            d: "amarillo"
-        },
-        correctAnswers: ["b"],
-        questionType: "single-choice"
-    },
-    {
-        question: "¿de qué color es el plátano?2",
-        answers: {
-            a: "verde",
-            b: "azul",
-            c: "naranja",
-            d: "amarillo"
-        },
-        correctAnswers: ["b"],
-        questionType: "single-choice"
-    },
-    {
-        question: "¿de qué color es el plátano?3",
-        answers: {
-            a: "verde",
-            b: "azul",
-            c: "naranja",
-            d: "amarillo"
-        },
-        correctAnswers: ["b"],
-        questionType: "single-choice"
-    },
-    {
-        question: "¿de qué color es el plátano?4",
-        answers: {
-            a: "verde",
-            b: "azul",
-            c: "naranja",
-            d: "amarillo"
-        },
-        correctAnswers: ["b"],
-        questionType: "single-choice"
-    },
-    // Agrega más preguntas aquí
-];
+import {myQuestions} from "/js/quizdata.js"; //contenido del cuestionario.
 
 let currentQuestion = 0;
 let userAnswers = [];
 
-const sliderContainer = document.querySelector('.slider-container');
+//objetos para manipular el DOM
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 
+//Constructor de preguntas
+//
 function buildQuestion(myQuestions, currentQuestion) {
     slides.forEach((slide, index) => {
         const question = myQuestions[currentQuestion];
         slide.innerHTML = `
-            <div class="">
+            <div class="" data-question=""${currentQuestion}>
             </div>
 
             <div>
@@ -92,7 +35,7 @@ function buildQuestion(myQuestions, currentQuestion) {
                             </li>
                         `).join('')}
                     </ul>
-                    <button type="submit" >COMPROBAR</button>
+                    <button type="submit">COMPROBAR</button>
                 </form>
             </div>
         `;
@@ -107,10 +50,6 @@ function buildQuestion(myQuestions, currentQuestion) {
              const selectedAnswers = formData.getAll('answers');
              console.log(selectedAnswers);
          });
-
-
-
-
 
     });
 }
